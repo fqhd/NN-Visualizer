afa9512bf7b434521c29f0dbc8b1942d85727b01
Fahd <fahd31256@gmail.com>
2023-01-31 14:45:10 +0000

    refactor: Renderer Class

--- script.js
+++ script.js
@@ -1,51 +1,14 @@
-let gl;
-const GRID_WIDTH = 10;
-
-// Scene
+let renderer;
 
 function run() {
-	initWebGL();
-
-	const grid = createGridModel();
-
-	// Create Plane VAO
-
+    renderer = new Renderer();
 	render();
-}
-
-function createGridModel(gl) {
-	const vertices = [];
-
-	// It's going to be a quad for now
-	vertices.push(-1, 0, -1);
-	vertices.push(-1, 0, 1);
-	vertices.push(1, 0, 1);
-	vertices.push(-1, 0, -1);
-	vertices.push(1, 0, 1);
-	vertices.push(1, 0, -1);
-
-	return Model(gl, vertices);
-
-	/*
-	for(let i = 0; i < GRID_WIDTH; i++) {
-		for(let j = 0; j < GRID_WIDTH; j++) {
-			
-		}
-	}
-	*/
-}
-
-function initWebGL() {
-	canvas = document.getElementById('Display');
-	gl = canvas.getContext('webgl');
 }
 
 function render() {
-
+    renderer.update();
+    renderer.render();
 	requestAnimationFrame(render);
 }
 
-window.onload = () => {
-	init();
-	render();
-}
+window.onload = run;
