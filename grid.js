2b68c0264824cf51dc9a83e7a06d6851d8fdbeb3
Fahd <fahd31256@gmail.com>
2023-01-31 19:05:27 +0000

    feat: draw grid

---
+++ grid.js
@@ -0,0 +1,49 @@
+class Grid {
+	constructor(gl) {
+		this.gl = gl;
+		this.createVBO();
+		this.createShader();
+	}
+
+	createVBO() {
+		const vertices = [];
+    
+        vertices.push(-1, 0, -1);
+        vertices.push(-1, 0, 1);
+        vertices.push(1, 0, 1);
+        vertices.push(-1, 0, -1);
+        vertices.push(1, 0, 1);
+        vertices.push(1, 0, -1);
+
+		this.vbo = this.gl.createBuffer();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
+		this.gl.bufferData(this.gl.ARRAY_BUFFER, new Float32Array(vertices), this.gl.STATIC_DRAW);
+	}
+
+	createShader() {
+		const vs = `
+			attribute vec3 aPosition;
+
+			void main(){
+				gl_Position = vec4(aPosition, 1.0);
+			}
+		`;
+		
+		const fs = `
+			void main(){
+				gl_FragColor = vec4(1.0);
+			}
+		`;
+
+		this.shader = new Shader(this.gl, vs, fs);
+	}
+
+	draw() {
+		this.shader.bind();
+		this.gl.bindBuffer(this.gl.ARRAY_BUFFER, this.vbo);
+		const attribLoc = this.gl.getAttribLocation(this.shader.program, 'aPosition');
+		this.gl.vertexAttribPointer(attribLoc, 3, this.gl.FLOAT, false, 0, 0);
+		this.gl.enableVertexAttribArray(attribLoc);
+		this.gl.drawArrays(this.gl.TRIANGLES, 0, 6);
+	}
+}
---
+++ renderer.js
@@ -0,0 +1,28 @@
+const GRID_WIDTH = 10;
+
+class Renderer {
+    constructor() {
+        this.gl = this.initWebGL();
+        this.grid = new Grid(this.gl);
+    }
+
+    update() {
+        // Updates the camera
+    }
+
+    render(nn) {
+		this.gl.clear(this.gl.COLOR_BUFFER_BIT | this.gl.DEPTH_BUFFER_BIT);
+		this.grid.draw();
+        // Draw Neural Network Plane
+    }
+
+    initWebGL() {
+        const canvas = document.getElementById('Display');
+        const gl = canvas.getContext('webgl');
+		gl.clearColor(0.0, 0.0, 0.0, 1.0);
+		gl.enable(gl.DEPTH_TEST);
+		gl.depthFunc(gl.LEQUAL);
+		gl.clearDepth(1.0);
+		return gl;
+    }
+}
